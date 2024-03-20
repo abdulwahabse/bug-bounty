8917221f98851cf7a8112229d498eaba8316fb13
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-20 12:29:59 +0100

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from "@radix-ui/themes"
 
 export default function IssuePage() {
   return (
     <div>
-      Issue Page
+      <Button>New Issue</Button>
     </div>
   )
 }
