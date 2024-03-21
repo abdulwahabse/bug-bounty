7b0a0accb101c4893ca4eb103a4a180dfec70aba
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-21 16:14:30 +0100

    Add loading skeletons for issue details and new issue pages

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,14 +1,14 @@
-import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import IssueStatusBadge from './../../components/IssueStatusBadge';
+import prisma from '@/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
-
+import IssueStatusBadge from './../../components/IssueStatusBadge';
 interface Props {
     params: { id: string }
 }
 
 export default async function IssueDetailPage({ params }: Props) {
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id)}
   })
