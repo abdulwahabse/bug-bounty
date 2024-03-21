4dd1f0bafb9a15d0e3d68d0c3eb5fa6da65a2fbb
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-21 18:17:54 +0100

    Build an API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ErrorMessage, Skeleton, Spinner } from '@/app/components'
-import { createIssueSchema } from '@/app/validationSchemas'
+import { issueSchema } from '@/app/validationSchemas'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Issue } from '@prisma/client'
 import { Button, Callout, TextField } from '@radix-ui/themes'
@@ -22,7 +22,7 @@ const DynamicSimpleMDE = dynamic(
     }
 )
 
-type IssueFormType = z.infer<typeof createIssueSchema>
+type IssueFormType = z.infer<typeof issueSchema>
 
 interface Props {
     issue?: Issue
@@ -31,7 +31,7 @@ interface Props {
 export default function IssueForm({ issue }: Props) {
     const router = useRouter()
     const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormType>({
-        resolver: zodResolver(createIssueSchema)
+        resolver: zodResolver(issueSchema)
     })
     const [error, setError] = useState('')
     const [isSubmitting, setSubmitting] = useState(false)
