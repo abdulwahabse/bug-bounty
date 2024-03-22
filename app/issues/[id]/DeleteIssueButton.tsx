d40721a6d21235dd949ed245c907ea09f91b23d0
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-22 10:44:27 +0100

    Show a confirmation dialog box on deleting an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 
 interface Props {
     issueId: number
@@ -6,8 +8,24 @@ interface Props {
 
 export default function DeleteIssueButton({ issueId }: Props) {
     return (
-        <Button color='red'>
-            Delete Issue
-        </Button>
+        <AlertDialog.Root> 
+            <AlertDialog.Trigger>
+                <Button color='red'>Delete Issue</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Delete Issue?</AlertDialog.Title>
+                <AlertDialog.Description>
+                    This issue will be permanently deleted from the database.
+                </AlertDialog.Description>
+            <Flex mt='4' gap='3'>
+                <AlertDialog.Cancel>
+                    <Button variant='soft' color='gray'>Cancel</Button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action>
+                    <Button color='red'>Delete</Button>
+                </AlertDialog.Action>
+            </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     )
 }
