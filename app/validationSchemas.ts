bf15dde8e0d67d539e0400478482ed5b6d406448
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-23 14:49:47 +0100

    Enhance the API to assign issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,31 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const issueSchema = z.object({
-    title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required.')
+    title: z
+        .string()
+        .min(1, 'Title is required.')
+        .max(255),
+    description: z
+        .string()
+        .min(1, 'Description is required.')
+        .max(65535, 'Description exceeded the string limit.')
+})
+
+export const patchIssueSchema = z.object({
+    title: z
+        .string()
+        .min(1, 'Title is required.')
+        .max(255)
+        .optional(),
+    description: z
+        .string()
+        .min(1, 'Description is required.')
+        .max(65535, 'Description exceeded the limit.')
+        .optional(),
+    assignedToUserId: z
+        .string()
+        .min(1, 'AssignedToUserId is required.')
+        .max(255, 'AssignedToUserId exceeded the string limit')
+        .optional()
+        .nullable()
 });
